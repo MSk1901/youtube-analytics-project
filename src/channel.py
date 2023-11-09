066d54b422f2e15ed005feda251145fc70ef7ee2
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-11-09 18:14:20 +0300

    add new funcs

--- src/channel.py
+++ src/channel.py
@@ -3,23 +3,55 @@ import os
 
 from googleapiclient.discovery import build
 
-api_key: str = os.getenv('YT_API_KEY')
-youtube = build('youtube', 'v3', developerKey=api_key)
-
 
 class Channel:
     """Класс для ютуб-канала"""
+
+    @property
+    def channel_id(self) -> str:
+        """Возвращает id канала для работы с ним"""
+        return self.__channel_id
 
     def __init__(self, channel_id: str) -> None:
         """
         Экземпляр инициализируется id канала.
         Дальше все данные будут подтягиваться по API.
         """
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+
+        channel = (self.get_service().channels().list
+                   (id=self.channel_id, part="snippet,statistics").execute())
+
+        self.title = channel["items"][0]["snippet"]["title"]
+        self.description = channel["items"][0]["snippet"]["description"]
+        self.url = f"https://www.youtube.com/channel/{self.channel_id}"
+        self.subscriber_count = (
+            channel)["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = channel["items"][0]["statistics"]["viewCount"]
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
+        youtube = self.get_service()
         channel = (youtube.channels().
                    list(id=self.channel_id,
                         part='snippet,statistics').execute())
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с YouTube API"""
+        api_key: str = os.getenv('YT_API_KEY')
+        return build('youtube', 'v3', developerKey=api_key)
+
+    def to_json(self, filename: str) -> None:
+        """Сохраняет в файл значения атрибутов экземпляра Channel"""
+        channel_info = {"title": self.title,
+                        "channel_id": self.channel_id,
+                        "description": self.description,
+                        "url": self.url,
+                        "subscriber_count": self.subscriber_count,
+                        "video_count": self.video_count,
+                        "view_count": self.view_count}
+        with open(filename, 'w', encoding='utf-8') as file:
+            json.dump(channel_info, file, indent=4, ensure_ascii=False)
