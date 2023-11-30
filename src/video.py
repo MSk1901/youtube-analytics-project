433031c5ac36018e179acb29039f157b75be8d02
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-11-30 12:51:46 +0300

    add try except blocks

--- src/video.py
+++ src/video.py
@@ -6,24 +6,28 @@ api_key: str = os.getenv('YT_API_KEY')
 
 
 class Video:
-
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id):
         self.video_id = video_id
-        video_response = self.youtube.videos().list(part='snippet,statistics,'
-                                                    'contentDetails,'
-                                                    'topicDetails',
-                                               id=video_id
-                                               ).execute()
-        print(video_response)
-        self.video_title = video_response['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/watch?v={self.video_id}"
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+        try:
+            video_response = self.youtube.videos().list(part='snippet,statistics,'
+                                                             'contentDetails,'
+                                                             'topicDetails',
+                                                        id=video_id
+                                                        ).execute()
+            self.title = video_response['items'][0]['snippet']['title']
+            self.url = f"https://www.youtube.com/watch?v={self.video_id}"
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except Exception:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
